b4a1f06aef028485c85c7a49734ccace398bbcaa
Kura676 <147655739+Kura676@users.noreply.github.com>
2023-11-08 23:03:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    while( in_array( ($a = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($b = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($c = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($d = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($e = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($f = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($g = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($h = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($i = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($j = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($k = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($l = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($m = mt_rand(-5, 6)), array(0) ) );
+    while( in_array( ($n = mt_rand(-5, 6)), array(0) ) );
+   
+    $result = "ur($a, $b) L = ($c, $d) UL = ($e, $f) ur($g, $h) L = ($i, $j) UL = ($k, $l) \ = ($m, $n)";
+echo $result;
+  ?>
+
+</body>
+</html>
